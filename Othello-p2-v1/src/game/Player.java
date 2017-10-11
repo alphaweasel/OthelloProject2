c9ca12bfcfa8d890c4106c17df642972c95fc915
Garrison Henkle <alphaweasel@users.noreply.github.com>
2017-10-11 17:12:49 -0500

    Working on setting mode for the game and getting user input fixed

--- Othello-p2-v1/src/game/Player.java
+++ Othello-p2-v1/src/game/Player.java
@@ -1,5 +1,7 @@
 package game;
 
+import java.util.Scanner;
+
 /**
  * Contains all of the user interface for the game, such as input and player
  * actions
@@ -15,7 +17,95 @@ package game;
  *
  */
 public class Player {
-	public void getMove(){
-	
+
+	// constants
+	private final String B = "Black";
+	private final String W = "White";
+
+	// variables
+	String color;
+	int rowIn;
+	int colIn;
+	int xCoord;
+	int yCoord;
+	String input;
+	String gameMode;
+	int simNumber;
+
+	public Player(String colorIn) {
+		color = colorIn;
+	} // end constructor Player(String)
+
+	public void setMode() {
+		Scanner userIn = new Scanner(System.in);
+
+		System.out.println("Select mode: ");
+		System.out.println("Enter \"1p\" for one player, \"2p\" for two player, or a \n"
+				+ "number n to run a simulated game between two bots n times");
+
+		int loop = 1;
+		String mode = null;
+
+		while (loop == 1) {
+			try {
+				mode = userIn.nextLine();
+			} catch (Exception ex) {
+				System.out.println("Invalid, please try again:");
+			} finally {
+				userIn.close();
+			}
+			if (parseMode(mode) != 0)
+				loop = 0;
+		} // end while loop
+		
+		switch(parseMode(mode)) {
+			case -1:
+				gameMode = "1 Player";
+				break;
+			case -2:
+				gameMode = "2 Player";
+				break;
+			default:
+				gameMode = "Simulator";
+				simNumber = parseMode(mode);
+				break;
+		} //end switch statement
+		
+	} // end method setMode
+
+	public void getMove() {
+		System.out.println("");
 	}
+
+	/**
+	 * Parses the input stringIn and returns a selected mode as an integer
+	 * 
+	 * 0 indicates an invalid mode, negative numbers indicate the amount of players,
+	 * and positive numbers indicate how many times a simulation will be run
+	 * 
+	 * @param stringIn
+	 *            userInput that is parsed for mode information
+	 * @return type int that indicates what mode was selected
+	 */
+	public int parseMode(String stringIn) {
+		int type = 0;
+		try {
+			int hopefullyANumber = Integer.parseInt(stringIn);
+			if (hopefullyANumber > 0)
+				type = hopefullyANumber;
+		} catch (NumberFormatException ex) {
+			type = 0;
+		}
+
+		if (type == 0) {
+			if (stringIn.equals("p1"))
+				type = -1;
+			else if (stringIn.equals("p2"))
+				type = -2;
+			else
+				type = 0;
+		}
+		return type;
+	}
+
 }
